46c3aad64b8ec2b6dfa618776abdb352e321ff08
maret.olia.00@gmail.com <maretAZA00>
2016-12-12 15:51:37 -0800

    initial commit

---
+++ src/main/java/giraffe/auth/GiraffeAuthServerApplication.java
@@ -0,0 +1,12 @@
+package giraffe.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiraffeAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiraffeAuthServerApplication.class, args);
+	}
+}
---
+++ src/test/java/giraffe/auth/GiraffeAuthServerApplicationTests.java
@@ -0,0 +1,16 @@
+package giraffe.auth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GiraffeAuthServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
